409292c99752e1f018f070ad80bcaee540de7f7a
Maitena Escalada <maitena_escalada@hotmail.com>
2024-12-18 01:07:22 -0500

    * Update: script.js  + Agregue verificacion para saber si un producto se agrego al carrito

--- js/script.js
+++ js/script.js
@@ -3,8 +3,14 @@ let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
 // función para añadir productos al carrito
 const agregarAlcarrito = (nombre, precio) => {
-    // agregar el producto como un objeto al carrito
-    carrito.push({ nombre, precio });
+    const productoExistente = carrito.find(item => item.nombre === nombre);
+
+    // si el producto ya existe, aumentar la cantidad
+    if (productoExistente) {
+        productoExistente.cantidad += 1;
+    } else {
+        carrito.push({ nombre, precio, cantidad: 1 });
+    }
 
     // Guarda el contenido del carrito en el almacenamiento local
     localStorage.setItem('carrito', JSON.stringify(carrito));
@@ -19,7 +25,8 @@ const agregarAlcarrito = (nombre, precio) => {
 // función para actualizar el contador del carrito
 const actualizarContador = () => {
     // cambiamos el contenido del HTML con el ID contador-carrito
-    document.getElementById("contador-carrito").textContent = carrito.length;
+    const total = carrito.reduce((contador, item) => contador + item.cantidad, 0);
+    document.getElementById("contador-carrito").textContent = total;
 }
 
 // función para cargar el contenido del carrito al iniciar la página
@@ -28,5 +35,5 @@ const cargarCarrito = () => {
     actualizarContador();
 }
 
-// llama a la función para cargar el carrito al cargar la página
+// Llama a la función para cargar el carrito al cargar la página
 document.addEventListener('DOMContentLoaded', cargarCarrito);
