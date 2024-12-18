3d9f641a638d49f92bb9b7310accb51b32049e3a
Maitena Escalada <maitena_escalada@hotmail.com>
2024-12-17 19:52:25 -0500

    *Add - productos.js con array de productos

---
+++ js/productos.js
@@ -0,0 +1,47 @@
+// array de productos
+const productos = [
+    {
+        "id": 1,
+        "nombre": "Torta de Margaritas",
+        "descripcion": "La torta puede ser de 12 a 20 porciones. El bizcocho como el relleno es a pedido del cliente. Puede colocarse una dedicatoria, esto tiene un adicional de $2.",
+        "precio": 150,
+        "imagen": "../img/margaritas.jpg"
+    },
+    {
+        "id": 2,
+        "nombre": "Torta arcoiris",
+        "descripcion": "La torta puede ser de 12 a 20 porciones. El bizcocho como el relleno es a pedido del cliente. La decoración se hace con Buttercream. Puede colocarse una dedicatoria, esto tiene un adicional de $2.",
+        "precio": 100,
+        "imagen": "../img/calado-costado.png"
+    },
+    {
+        "id": 3,
+        "nombre": "Torta Butterfly",
+        "descripcion": "La torta puede ser de 20 a 25 porciones. El bizcocho como el relleno es a pedido del cliente. La decoración se hace con Buttercream. Puede colocarse una dedicatoria, esto tiene un adicional de $2.",
+        "precio": 200,
+        "imagen": "../img/torta-rosada-mariposas.jpg"
+    },
+    {
+        "id": 4,
+        "nombre": "Torta Elvis",
+        "descripcion": "La torta puede ser de 20 a 25 porciones. El bizcocho como el relleno es a pedido del cliente. La decoración se hace con Buttercream. Puede colocarse una dedicatoria, esto tiene un adicional de $2.",
+        "precio": 200,
+        "imagen": "../img/torta-cuy.jpg"
+    },
+    {
+        "id": 5,
+        "nombre": "Molde de galletas - Abecedario",
+        "descripcion": "Juego de molde para realizar galletas con forma de letras.",
+        "precio": 10,
+        "imagen": "../img/molde-galletas.jpg"
+    },
+    {
+        "id": 6,
+        "nombre": "Torta ChocolateLover",
+        "descripcion": "La torta puede ser de 12 a 20 porciones. El bizcocho como el relleno es a pedido del cliente. La decoración se hace con frosting. Puede colocarse una dedicatoria, esto tiene un adicional de $2.",
+        "precio": 200,
+        "imagen": "../img/chocolate-lover.jpg"
+    },
+];
+
+
